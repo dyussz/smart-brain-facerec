8dca639c5cf52dd924099188dadc0ba47059c256
Zuriya Dyussebayeva <zuriya.dyusebaeva@gmail.com>
2025-09-29 23:20:25 +0200

    muted css errors

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {}
-  }
+  },
+    esbuild: {
+        logOverride: {
+            'css-syntax-error': 'silent'
+        }
+    }
 });
